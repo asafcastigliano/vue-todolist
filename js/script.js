cee413d78370533c24ee25f348f4f5737115d34d
asafcastigliano <asafcastigliano@gmail.com>
2023-12-13 16:44:59 +0100

    finita struttura pagina e inziata parte della stampa

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            todolist: [
+                {
+                    todo: "Scendere il cane",
+                    done: false
+                },
+                {
+                    todo: "Pisciare il cane",
+                    done: false
+                },
+            ]
         }
     },
 
